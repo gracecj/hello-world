ec63791344f1de8b464a933666e8a62bfedef881
gracecj <69876367+gracecj@users.noreply.github.com>
2020-08-18 18:32:44 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*******************************************
+* Hello.java
+********************************************
+* Grace Johnson
+* 8/18/20
+* 255 04
+********************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("Grace Johnson");
+	System.out.println("Hello.java");
+	System.out.println("My first Java project");
+	System.out.println("8/18/20");
+	System.out.println("255 004");
+	System.out.println();
+
+	}
+}
